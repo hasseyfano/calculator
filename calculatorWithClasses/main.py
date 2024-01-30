6f1357699325064f0561db9c3c2973d9725a2fae
hasseyfano <hasseymoi@gmail.com>
2024-01-30 15:58:28 -0500

    added a folder and devide the whole file into chunks

---
+++ calculatorWithClasses/main.py
@@ -0,0 +1,33 @@
+# main_calculator.py
+from calculator import Calculator
+
+def run_calculator():
+    calculator = Calculator()
+
+    print("Select operation.")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    while True:
+        # take input from the user
+        choice = input("Enter choice (1/2/3/4): ")
+
+        # check if choice is one of the four options
+        if choice in ('1', '2', '3', '4'):
+            num1, num2 = calculator.get_user_input()
+
+            result = calculator.perform_calculation(choice, num1, num2)
+            print(result)
+
+            # check if user wants another calculation
+            # break the while loop if the answer is no
+            next_calculation = input("Do you want to perform another calculation? (yes/no): ")
+            if next_calculation.lower() != "yes":
+                break
+        else:
+            print("Invalid Input")
+
+if __name__ == "__main__":
+    run_calculator()
